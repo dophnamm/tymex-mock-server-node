1c89e2553f26ba3a0a268e6ee63652836d2e48c0
Phuong Nam <dophnamm@gmail.com>
2024-12-22 13:57:04 +0700

    chore: fix cors

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 const port = 5005;
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
